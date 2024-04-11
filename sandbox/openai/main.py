1f099e23f7ad988deb65f60e6dad409dace60fdc
shoji kobayashi <shoji.kobayashi.0125@gmail.com>
2024-04-11 05:32:45 +0000

    add openai sample

---
+++ sandbox/openai/main.py
@@ -0,0 +1,15 @@
+import os
+from openai import OpenAI
+
+client = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY"),
+)
+model = "gpt-3.5-turbo"
+
+stream = client.chat.completions.create(
+    model=model,
+    messages=[{"role": "user", "content": "Give me a number out of 10"}],
+    stream=True,
+)
+for chunk in stream:
+    print(chunk.choices[0].delta.content or "", end="")
